ab5c2492aad97cfe72e863fe5c5991564f58f533
Katrine <katrine.bjormyr@gmail.com>
2024-05-15 07:59:13 -0500

    script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+// menu function
+function myMenuFunction() {
+    var i = document.getElementById("navMenu");
+
+    if(i.className === "nav-menu"){
+        i.className += " responsive";
+    } else {
+        i.className = "nav-menu";
+    }
+}
+
